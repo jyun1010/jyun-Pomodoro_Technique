8c34c63b12fe44e517b9496f5661e0c48b2c96f3
jyun1010 <45022273+jyun1010@users.noreply.github.com>
2019-07-19 21:36:20 +0800

    js新增

--- js/js.js
+++ js/js.js
@@ -1,16 +1,37 @@
 $(document).ready(function () {
 
-    // var isStop = false, isBind = false;
+    var isStop = false, isBind = false;
 
     //番茄鐘頁
-
+    var timer2 = "25:00";
     $(".play_btn").click(function () {
         if ($(".play_btn").attr("src") === "images/play_white.svg") {
             $(".play_btn").attr("src", "images/pause_circle.svg");
-           
+            // $(".play_btn").attr("src", "images/pause_circle.svg");
+            isStop = false;
+            if (!isBind) {
+                isBind = true;
+                var interval = setInterval(function () {
+
+                    if (!isStop) {
+                        var timer = timer2.split(':');
+                        //by parsing integer, I avoid all extra string processing
+                        var minutes = parseInt(timer[0], 10);
+                        var seconds = parseInt(timer[1], 10);
+                        --seconds;
+                        minutes = (seconds < 0) ? --minutes : minutes;
+                        if (minutes < 0) clearInterval(interval);
+                        seconds = (seconds < 0) ? 59 : seconds;
+                        seconds = (seconds < 10) ? '0' + seconds : seconds;
+                        //minutes = (minutes < 10) ?  minutes : minutes;
+                        $('.f_time').text(minutes + ':' + seconds);
+                        timer2 = minutes + ':' + seconds;
+                    }
+                }, 1000);
+            }
             $(".cd").css("animation", "count_color 5s infinite");
         } else {
-      
+            isStop = true;
             $(".play_btn").attr("src", "images/play_white.svg");
             $(".cd").css("animation", "none");
         }
@@ -19,14 +40,38 @@ $(document).ready(function () {
 
 
     //休息頁
-
+    var timer3 = "05:00";
     $(".play_btn_b").click(function () {
         if ($(".play_btn_b").attr("src") === "images/play_white.svg") {
             $(".play_btn_b").attr("src", "images/pause_circle.svg");
+            // $(".play_btn").attr("src", "images/pause_circle.svg");
+            isStop = false;
+            if (!isBind) {
+                isBind = true;
+                var interval = setInterval(function () {
+
+                    if (!isStop) {
+                        var timer = timer3.split(':');
+                        //by parsing integer, I avoid all extra string processing
+                        var minutes = parseInt(timer[0], 10);
+                        var seconds = parseInt(timer[1], 10);
+                        --seconds;
+                        minutes = (seconds < 0) ? --minutes : minutes;
+                        if (minutes < 0) clearInterval(interval);
+                        seconds = (seconds < 0) ? 59 : seconds;
+                        seconds = (seconds < 10) ? '0' + seconds : seconds;
+                        //minutes = (minutes < 10) ?  minutes : minutes;
+                        $('.f_time_b').text(minutes + ':' + seconds);
+                        timer3 = minutes + ':' + seconds;
+                    }
+                }, 1000);
+
+            }
             $(".cd_b").css("animation", "count_color_b 5s infinite");
 
+
         } else {
-        
+            isStop = true;
             $(".play_btn_b").attr("src", "images/play_white.svg");
             $(".cd_b").css("animation", "none");
         }
